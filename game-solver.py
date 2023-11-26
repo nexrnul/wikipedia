4884d0c01fdd3593ff7f50ecaa03395f61075db8
nexer nul <redpenguin4luca26@gmail.com>
2023-11-26 03:48:35 +0000

    bsf

--- game-solver.py
+++ game-solver.py
@@ -19,15 +19,55 @@ def fetch_links(page):
 def wikipedia_game_solver(start_page, target_page):
     print('Working on it...')
     start_time = time.time()
+
+    visited = []
+    queue = Queue()
+    path = []
+    parent = {}
+
+    queue.put(start_page.title)
+    visited.append(start_page.title)
+#Breadth first search
+    while not queue.empty():
+
+        current_title = queue.get()
+
+        if current_title == target_page.title:
+            break
+
+        visited.append(current_title)
+        current_page = wiki_wiki.page(current_title)
+        next_level = fetch_links(current_page)
+
+        for node in next_level:
+            if node not in visited:
+                queue.put(node)
+                parent[node] = current_title
+    child = target_page.title
+    while child != start_page.title:
+        path.append(child)
+        child = parent[child]
+    path.append(start_page.title)
+    path.reverse()
+
+
+    end_time = time.time()
+    print("this algorithm took", end_time-start_time, "seconds to run")
+
+    return path
+
+    
+    
   
-    # FINISH THE CODE HERE
+
+
 
     end_time = time.time()
     print("This algorithm took", end_time-start_time, "seconds to run!")
   
     return path
 
-# Example usage:
+# usage
 start_page = wiki_wiki.page('Nina Tandon')
 target_page = wiki_wiki.page('Italian language')
 path = wikipedia_game_solver(start_page, target_page)
