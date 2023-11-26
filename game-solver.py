404e7e427c3646ecc159494507e7a86baf7c8bd8
nexer nul <redpenguin4luca26@gmail.com>
2023-11-26 05:22:08 +0000

    broken

--- game-solver.py
+++ game-solver.py
@@ -6,7 +6,7 @@ user_agent = "MsOrret'sWikipediaGame/1.0 (orret.deborah@pusd.us)"
 
 wiki_wiki = wikipediaapi.Wikipedia(user_agent, "en")
 
-# HELPER FUNCTION: fetch_links(page) passes in a wikipedia page and returns a list of all the pages linked from that page
+# passes wikipedia page and returns a list of all pages linked on starting page
 def fetch_links(page):
     links_list = []
     links = page.links
@@ -27,9 +27,8 @@ def wikipedia_game_solver(start_page, target_page):
 
     queue.put(start_page.title)
     visited.append(start_page.title)
-#Breadth first search
+#breadth first search traversal
     while not queue.empty():
-
         current_title = queue.get()
 
         if current_title == target_page.title:
@@ -38,37 +37,30 @@ def wikipedia_game_solver(start_page, target_page):
         visited.append(current_title)
         current_page = wiki_wiki.page(current_title)
         next_level = fetch_links(current_page)
-
+#solution to multiple nodes being prevelant (speeds up process)
         for node in next_level:
             if node not in visited:
                 queue.put(node)
                 parent[node] = current_title
+#determination             
     child = target_page.title
     while child != start_page.title:
         path.append(child)
         child = parent[child]
     path.append(start_page.title)
     path.reverse()
-
-
-    end_time = time.time()
-    print("this algorithm took", end_time-start_time, "seconds to run")
+    end_time = time.time()    
+    print("this traversal took", end_time-start_time, "seconds to run")
 
     return path
-
     
-    
-  
 
-
-
-    end_time = time.time()
-    print("This algorithm took", end_time-start_time, "seconds to run!")
-  
-    return path
-
-# usage
-start_page = wiki_wiki.page('Nina Tandon')
-target_page = wiki_wiki.page('Italian language')
+# results
+start_page = wiki_wiki.page('Ogdoad (Gnosticism)')
+target_page = wiki_wiki.page('Seven heavens')
 path = wikipedia_game_solver(start_page, target_page)
-print("Shortest path:", path)
+print("you traveled from: %s" % start_page.title)
+print("all the way to: %s" % target_page.title)
+print("brief summary: %s" % target_page.summary[0:20])
+print("shortest pathway:", path)
+
